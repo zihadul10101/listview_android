7199454ee265f21609c267b2d8c6c0e22b70b3f9
zihad <zihadul10101@gmail.com>
2023-09-03 00:31:11 +0600

    listview with adapter with setonitemlistener

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
         String[] countryName=getResources().getStringArray(R.array.country_name);
         ArrayAdapter<String> adapter=new ArrayAdapter<String>(MainActivity.this,R.layout.sample_view,R.id.textView,countryName);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String value=countryName[i];
+                Toast.makeText(MainActivity.this,value+""+i,Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
